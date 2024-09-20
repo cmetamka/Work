a4f61c3f0276a3a3452df461623c949292644bb9
Daniil Martyshev <martyshev222@mail.ru>
2024-09-20 23:17:49 +0300

    Nachalo

---
+++ task1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Квартира стоит-13.000.000
+//Alice 
+//  взнос 1.000.000, 17% годовых, 30 ипотека
+//  зарплата 200.000
+//  траты в месяц 50.000
+//Bob
+//  30.000 аренда
+//  Траты в месяц -50.000
+//  зарплата 200.000
+//inflation- 9%
+
+
+int main() {
+
+    int year = 2024;
+    float infl = 0.09;
+
+    int Alice_dom = 13000000;
+    int Alice_capital= 0;
+    
+    int Bob_bank = 1000000; 
+    int Bob_capital = 0;
+
+    
+}
